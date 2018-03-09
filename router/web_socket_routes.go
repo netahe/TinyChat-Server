fa608d09768f3a3436b3bed9608df1a8c94ade3a
Neta Hemyan <neta.he@gmail.com>
2018-03-09 17:32:07 +0200

    websocket routes implementation

--- router/web_socket_routes.go
+++ router/web_socket_routes.go
@@ -3,18 +3,49 @@ package router
 import (
 	"github.com/gorilla/websocket"
 	"net/http"
+	"log"
+	"github.com/netahe/TinyChat-Server/updater"
 	"github.com/gorilla/mux"
 )
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
 func TrackChannels(writer http.ResponseWriter, request *http.Request) {
-	channel := mux.Vars(request)["chan_id"]
+	conn, err := upgrader.Upgrade(writer, request, nil)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	updater.SubscribeToChannelsUpdater(conn)
 
 }
 
 func TrackMessages(writer http.ResponseWriter, request *http.Request) {
 	channel := mux.Vars(request)["chan_id"]
+	conn, err := upgrader.Upgrade(writer, request, nil)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	updater.SubscribeToMessagesUpdater(channel, conn)
+
 }
 
 func TrackUsers(writer http.ResponseWriter, request *http.Request) {
 	channel := mux.Vars(request)["chan_id"]
+	conn, err := upgrader.Upgrade(writer, request, nil)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	updater.SubscribeToUsersUpdater(channel, conn)
 }
